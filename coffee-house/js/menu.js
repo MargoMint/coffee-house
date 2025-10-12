16392db426ace4605f5eb954c4d28bae040e9407
Marharyta <rita.shyshkavets@gmail.com>
2025-10-12 13:43:45 +0200

    feat: add product cards to the menu page

--- coffee-house/js/menu.js
+++ coffee-house/js/menu.js
@@ -1,31 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
   const menuLink = document.getElementById('menu-link');
+  const cardsSection = document.querySelector('.menu__cards');
 
-  if (window.location.pathname.includes('menu.html')) {
+  if (window.location.pathname.includes('menu.html') && menuLink) {
     menuLink.classList.add('header__menu--active');
   }
 
   setupTabs();
+
+  cardsSection.classList.add('menu-section--active');
+  renderCards(products.coffee);
 });
 
-function setupTabs(cards) {
+function setupTabs() {
   const tabs = document.querySelectorAll('.menu__tab');
-  const sections = document.querySelectorAll('.menu-section');
+  const cardsSection = document.querySelector('.menu__cards');
 
   tabs.forEach((tab) => {
     tab.addEventListener('click', () => {
       tabs.forEach((t) => t.classList.remove('menu__tab--active'));
       tab.classList.add('menu__tab--active');
-
-      sections.forEach((section) => {
-        section.classList.remove('menu-section--active');
-      });
-
       const categoryId = tab.id;
-      const activeSection = document.querySelector(`.${categoryId}`);
-      if (activeSection) {
-        activeSection.classList.add('menu-section--active');
-      }
+      cardsSection.classList.add('menu-section--active');
+      renderCards(products[categoryId]);
     });
   });
+}
+
+function renderCards(productsArray) {
+  const container = document.querySelector('.menu__cards');
+  container.innerHTML = '';
+
+  const cardsHTML = productsArray
+    .map(
+      (item) => `
+      <div class="menu-card">
+        <img src="${item.image}" alt="${item.name}" class="menu-card__img" />
+        <div class="menu-card__content">
+          <div class="menu-card__text">
+            <h3 class="menu-card__title">${item.name}</h3>
+            <p class="menu-card__desc">${item.description}</p>
+          </div>
+          <span class="menu-card__price">${item.price}</span>
+        </div>
+      </div>
+    `
+    )
+    .join('');
+
+  container.innerHTML = cardsHTML;
 }
---
+++ coffee-house/js/products.js
@@ -0,0 +1,158 @@
+const products = {
+  coffee: [
+    {
+      id: 1,
+      name: 'Irish coffee',
+      description:
+        'Fragrant black coffee with Jameson Irish whiskey and whipped milk',
+      price: '$7.00',
+      image: 'img/products/coffee/coffee-1.jpg',
+    },
+    {
+      id: 2,
+      name: 'Kahlua coffee',
+      description:
+        'Classic coffee with milk and Kahlua liqueur under a cap of frothed milk',
+      price: '$7.00',
+      image: 'img/products/coffee/coffee-2.jpg',
+    },
+    {
+      id: 3,
+      name: 'Honey raf',
+      description: 'Espresso with frothed milk, cream and aromatic honey',
+      price: '$5.50',
+      image: 'img/products/coffee/coffee-3.jpg',
+    },
+    {
+      id: 4,
+      name: 'Ice cappuccino',
+      description: 'Cappuccino with soft thick foam in summer version with ice',
+      price: '$5.00',
+      image: 'img/products/coffee/coffee-4.jpg',
+    },
+    {
+      id: 5,
+      name: 'Espresso',
+      description: 'Classic black coffee',
+      price: '$4.50',
+      image: 'img/products/coffee/coffee-5.jpg',
+    },
+    {
+      id: 6,
+      name: 'Latte',
+      description:
+        'Espresso coffee with the addition of steamed milk and dense milk foam',
+      price: '$5.50',
+      image: 'img/products/coffee/coffee-6.jpg',
+    },
+    {
+      id: 7,
+      name: 'Latte macchiato',
+      description: 'Espresso with frothed milk and chocolate',
+      price: '$5.50',
+      image: 'img/products/coffee/coffee-7.jpg',
+    },
+    {
+      id: 8,
+      name: 'Coffee with cognac',
+      description: 'Fragrant black coffee with cognac and whipped cream',
+      price: '$6.50',
+      image: 'img/products/coffee/coffee-8.jpg',
+    },
+  ],
+  tea: [
+    {
+      id: 1,
+      name: 'Moroccan',
+      description:
+        'Fragrant black tea with the addition of tangerine, cinnamon, honey, lemon and mint',
+      price: '$4.50',
+      image: 'img/products/tea/tea-1.jpg',
+    },
+    {
+      id: 2,
+      name: 'Ginger',
+      description: 'Original black tea with fresh ginger, lemon and honey',
+      price: '$5.00',
+      image: 'img/products/tea/tea-2.jpg',
+    },
+    {
+      id: 3,
+      name: 'Cranberry',
+      description: 'Invigorating black tea with cranberry and honey',
+      price: '$5.00',
+      image: 'img/products/tea/tea-3.jpg',
+    },
+    {
+      id: 4,
+      name: 'Sea buckthorn',
+      description:
+        'Toning sweet black tea with sea buckthorn, fresh thyme and cinnamon',
+      price: '$5.50',
+      image: 'img/products/tea/tea-4.jpg',
+    },
+  ],
+  dessert: [
+    {
+      id: 1,
+      name: 'Marble cheesecake',
+      description:
+        'Philadelphia cheese with lemon zest on a light sponge cake and red currant jam',
+      price: '$3.50',
+      image: 'img/products/dessert/dessert-1.jpg',
+    },
+    {
+      id: 2,
+      name: 'Red velvet',
+      description: 'Layer cake with cream cheese frosting',
+      price: '$4.00',
+      image: 'img/products/dessert/dessert-2.jpg',
+    },
+    {
+      id: 3,
+      name: 'Cheesecakes',
+      description:
+        'Soft cottage cheese pancakes with sour cream and fresh berries and sprinkled with powdered sugar',
+      price: '$4.50',
+      image: 'img/products/dessert/dessert-3.jpg',
+    },
+    {
+      id: 4,
+      name: 'Creme brulee',
+      description:
+        'Delicate creamy dessert in a caramel basket with wild berries',
+      price: '$4.00',
+      image: 'img/products/dessert/dessert-4.jpg',
+    },
+    {
+      id: 5,
+      name: 'Pancakes',
+      description: 'Tender pancakes with strawberry jam and fresh strawberries',
+      price: '$4.50',
+      image: 'img/products/dessert/dessert-5.jpg',
+    },
+    {
+      id: 6,
+      name: 'Honey cake',
+      description: 'Classic honey cake with delicate custard',
+      price: '$4.50',
+      image: 'img/products/dessert/dessert-6.jpg',
+    },
+    {
+      id: 7,
+      name: 'Chocolate cake',
+      description:
+        'Cake with hot chocolate filling and nuts with dried apricots',
+      price: '$5.50',
+      image: 'img/products/dessert/dessert-7.jpg',
+    },
+    {
+      id: 8,
+      name: 'Black forest',
+      description:
+        'A combination of thin sponge cake with cherry jam and light chocolate mousse',
+      price: '$6.50',
+      image: 'img/products/dessert/dessert-8.jpg',
+    },
+  ],
+};
